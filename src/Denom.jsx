ec82f8a22bb64dbdd1aabc568c627b9117e91c6e
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-02-29 23:50:01 +0100

    .

--- src/Denom.jsx
+++ src/Denom.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Denom = ({ state }) => (
   <div className="col-8">
     <div className="denomination-output twenties-output">{state.twenties}</div>
-    <div className="denomination-output tens-output">Tens: {state.tens}</div>
-    <div className="denomination-output fives-output">Fives: {state.fives}</div>
-    <div className="denomination-output ones-output">Ones: {state.ones}</div>
-    <div className="denomination-output quarters-output">Quarters: {state.quarters}</div>
-    <div className="denomination-output dimes-output">Dimes: {state.dimes}</div>
-    <div className="denomination-output nickels-output">Nickels: {state.nickels}</div>
-    <div className="denomination-output pennies-output">Pennies: {state.pennies}</div>
+    <div className="denomination-output tens-output">{state.tens}</div>
+    <div className="denomination-output fives-output">{state.fives}</div>
+    <div className="denomination-output ones-output">{state.ones}</div>
+    <div className="denomination-output quarters-output">{state.quarters}</div>
+    <div className="denomination-output dimes-output">{state.dimes}</div>
+    <div className="denomination-output nickels-output">{state.nickels}</div>
+    <div className="denomination-output pennies-output">{state.pennies}</div>
   </div>
 );
 
