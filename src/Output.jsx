14cee0e9b778aad96091380badacea867c1b63be
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-03-01 19:25:50 +0100

    .

--- src/Output.jsx
+++ src/Output.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Output = ({ changeDue }) => (
   <div className="col-8">
-    {changeDue > 0 && <div className="alert alert-success">Total change due: ${changeDue.toFixed(2)}</div>}
+    {changeDue > 0 && <div className="alert alert-success">The total change due is ${changeDue.toFixed(2)}</div>}
     {changeDue < 0 && <div className="alert alert-danger">Additional money owed: ${(changeDue * -1).toFixed(2)}</div>}
   </div>
 );
