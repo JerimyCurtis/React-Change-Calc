caa5edeb16d559660e4d971d35801bfc5d897c10
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-03-05 06:29:10 +0100

    Passes all tests, need denom lables

--- src/Denom.jsx
+++ src/Denom.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 const Denom = ({ state }) => (
   <div className="col-8">
-    <div className="denomination-output twenties-output">Twenties: {state.twenties || 0}</div>
-    <div className="denomination-output tens-output">Tens: {state.tens || 0}</div>
-    <div className="denomination-output fives-output">Fives: {state.fives || 0}</div>
-    <div className="denomination-output ones-output">Ones: {state.ones || 0}</div>
-    <div className="denomination-output quarters-output">Quarters: {state.quarters || 0}</div>
-    <div className="denomination-output dimes-output">Dimes: {state.dimes || 0}</div>
-    <div className="denomination-output nickels-output">Nickels: {state.nickels || 0}</div>
-    <div className="denomination-output pennies-output">Pennies: {state.pennies || 0}</div>
+    <div className="denomination-output change twenties-output">{state.twenties}</div>
+    <div className="denomination-output change tens-output">{state.tens}</div>
+    <div className="denomination-output change fives-output">{state.fives}</div>
+    <div className="denomination-output change ones-output">{state.ones}</div>
+    <div className="denomination-output change quarters-output">{state.quarters}</div>
+    <div className="denomination-output change dimes-output">{state.dimes}</div>
+    <div className="denomination-output change nickels-output">{state.nickels}</div>
+    <div className="denomination-output change pennies-output">{state.pennies}</div>
   </div>
 );
+
 
 export default Denom;
--- src/app.jsx
+++ src/app.jsx
@@ -7,8 +7,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      amountDue: ' ',
-      amountReceived: ' ', 
+      amountDue: '0',
+      amountReceived: '0', 
       changeDue: '0',
       twenties: '0',
       tens: '0',
@@ -28,37 +28,31 @@ class App extends Component {
   }
 
   calculateChange() {
-    const amountDue = parseFloat(this.state.amountDue) || 0;
-    const amountReceived = parseFloat(this.state.amountReceived) || 0;
-    let changeDue = amountReceived - amountDue;
-    
-    if (changeDue < 0) {
-      this.setState({ changeDue });
-      return;
-    }
-    
-    let remaining = Math.round(changeDue * 100);
-    const Denominations = {
-      twenties: 2000,
-      tens: 1000,
-      fives: 500,
-      ones: 100,
-      quarters: 25,
-      dimes: 10,
-      nickels: 5,
-      pennies: 1,
+    const amountDue = parseFloat(this.state.amountDue);
+    const amountReceived = parseFloat(this.state.amountReceived);
+    const changeDue = amountReceived - amountDue;
+    const remainingChange = Math.round(changeDue * 100);
+    let remaining = remainingChange;
+    const change = {
+      changeDue: changeDue.toFixed(2).toString(),
+      twenties: Math.floor(remaining / 2000).toString(),
     };
-    
-    let change = {changeDue: changeDue.toFixed(2)}; 
-    
-    Object.keys(Denominations).forEach(key => {
-      change[key] = Math.floor(remaining / Denominations[key]);
-      remaining %= parseFloat(Denominations[key]);
-    });
-  
+    remaining %= 2000;
+    change.tens = Math.floor(remaining / 1000).toString();
+    remaining %= 1000;
+    change.fives = Math.floor(remaining / 500).toString();
+    remaining %= 500;
+    change.ones = Math.floor(remaining / 100).toString();
+    remaining %= 100;
+    change.quarters = Math.floor(remaining / 25).toString();
+    remaining %= 25;
+    change.dimes = Math.floor(remaining / 10).toString();
+    remaining %= 10;
+    change.nickels = Math.floor(remaining / 5).toString();
+    remaining %= 5;
+    change.pennies = remaining.toString();
     this.setState(change);
-  }
-  
+  }  
 
   render() {
     console.log(this.state);
